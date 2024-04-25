0e12fd061039e2992d3e9ce9ae20977c8010c616
Hans Christian Ekne <hc.ekne@gmail.com>
2024-04-25 11:29:09 +0100

    updated r function split into function and test script

--- R/app/test_encoder.R
+++ R/app/test_encoder.R
@@ -2,12 +2,34 @@
 source("OneHotEncoder.R")
 
 
-# encoder <- OneHotEncoder()
+# Create an instance of my_class
+encoder <- OneHotEncoder()
 
-# # Fit the encoder with the training data
-# encoder$fit(training_data, cols = c("fuel_type", "color"))
+# Dummy data.table for demonstration
+training_data <- data.table(numerical_1 = c(1, 2, 3, 4, 5, 6, 7, 8),
+                 color_1_ = c("black", "black", "red", "green", "green", "black", "red", "blue"),
+                 color_2_ = c("black", "blue", "pink", "purple", "black", "blue", "pink", "purple"))
 
-# # Retrieve and print the categories
-# categories <- encoder$get_categories()
-# print("Categories learned:")
-# print(categories)
+
+# Dummy data.table for demonstration
+inference_data <- data.table(numerical_1 = c(11, 12, 13, 14, 15, 16, 17, 18),
+                  color_1_ = c("black", "blue","black", "green", "green", "black","black" ,"blue"),
+                  color_2_ = c("orange", "orange", "black", "orange","black", "orange", "orange", "orange"))
+
+print(inference_data)
+
+fit_columns = c("color_1_", "color_2_")
+# Use the fit method
+encoder$fit(dt=training_data, columns=fit_columns)
+
+# Print categories
+print(encoder$get_categories())
+
+# transform training
+
+transformed_training_data = encoder$transform(training_data)
+
+# transform inference
+
+transformed_inference_data = encoder$transform(inference_data)
+print(transformed_inference_data)
